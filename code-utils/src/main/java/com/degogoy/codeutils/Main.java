99cbafb376bf151a8e6ed97fac73653d056c2007
octaviodegodoy <octaviodegodoy@gmail.com>
2017-07-13 21:52:29 +0100

    test

--- code-utils/src/main/java/com/degogoy/codeutils/Main.java
+++ code-utils/src/main/java/com/degogoy/codeutils/Main.java
@@ -8,6 +8,6 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Beginning testing!" );
+        System.out.println( "Beginning testing !" );
     }
 }
